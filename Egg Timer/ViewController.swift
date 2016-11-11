2739d2d833796b9e165a9ba48484e2807ccf2a1a
sophea_chea <sophea@sopheas-Mac.local>
2016-11-12 01:53:50 +0700

    Finish Egg Timer

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -10,6 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    
+    var currentTimer = 0
+    var subTen = 0
+    var addTen = 0
+    
+    
+    @IBOutlet weak var lblDisplay: UILabel!
+    
+    @IBAction func btnPlay(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func btnPause(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func btnSubTen(_ sender: Any) {
+        if (Int(lblDisplay.text!)! > 10) {
+            subTen = Int(lblDisplay.text!)! - 10
+            lblDisplay.text = String(subTen)
+        }
+    }
+    
+    @IBAction func btnAddTen(_ sender: Any) {
+        addTen = Int(lblDisplay.text!)! + 10
+        lblDisplay.text = String(addTen)
+    }
+    
+    @IBAction func btnReset(_ sender: Any) {
+        lblDisplay.text = "210"
+    }
+    
+    func processTimer(){
+        if (Int(lblDisplay.text!)! > 0) {
+            currentTimer = Int(lblDisplay.text!)! - 1
+            lblDisplay.text = String(currentTimer)
+        }else{
+            timer.invalidate()
+            lblDisplay.text = "0"
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
